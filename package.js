337893a5c55925b32a91272754aa2d3102dac7c7
mrosstech <mrosstech@gmail.com>
2017-06-04 19:13:21 -0400

    Update package.js

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 {
-  "name"         : "node-web-scrape",
+  "name"         : "whatsforschoollunch",
   "version"      : "0.0.1",
   "description"  : "Scrape le web.",
-  "main"         : "server.js",
-  "author"       : "Scotch",
+  "main"         : "index.js",
+  "author"       : "mross",
   "dependencies" : {
     "express"    : "latest",
     "request"    : "latest",
