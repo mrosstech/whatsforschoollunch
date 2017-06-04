677e4b48af5db33753cb60900c82fc1fe43a01c8
mrosstech <mrosstech@gmail.com>
2017-06-04 19:14:15 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
 
+app.get('/scrape', function(req, res){
+
+  //All the web scraping magic will happen here
+
+})
+
+app.listen('8081')
+
+console.log('Magic happens on port 8081');
+
+exports = module.exports = app;
